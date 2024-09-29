bccd4fa13b30b156808db1015dec1adcbe0c7b08
JH <jhmediatech@outlook.com>
2024-09-28 21:54:11 -0400

    Completed reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/revealOnScroll';
+
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 85);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 70);
 
 let mobileMenu = new MobileMenu();
 

bf3671218faf72463d2bfdaeba3b2dae9fe6ca65
JH <jhmediatech@outlook.com>
2024-10-06 13:43:05 -0400

    Added modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,31 @@ import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/revealOnScroll';
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 85);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 70);
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+
+let modal;
+
+document.querySelectorAll('.open-modal').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (typeof modal == 'undefined') {
+      import('./modules/Modal')
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log('There was a problem!'));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();

c1c6e40382b66ece229cc0460c86c110e131bd82
JH <jhmediatech@outlook.com>
2024-10-05 20:09:11 -0400

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/revealOnScroll';
+
+let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 85);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 70);
